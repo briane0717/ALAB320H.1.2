ac93b8ebd5bc92a338d7d6c141c17a533ecb59bc
Brian <briane0717@gmail.com>
2024-12-15 08:39:38 -0500

    linked stylesheet

--- alab320h.1.2/src/App.jsx
+++ alab320h.1.2/src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Article from "./components/Article";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
